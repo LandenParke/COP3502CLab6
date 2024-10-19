962c9e935ff6d1da2e194b9e7810adb69c32ac49
LandenParke <eddisonparke@gmail.com>
2024-10-18 22:36:32 -0400

    Add files via upload

---
+++ encoder.py
@@ -0,0 +1,29 @@
+# Encoder() made by Landen Parke
+# parke.landen@ufl.edu
+
+
+# Program Functions
+
+def encode(password):
+    encoded_password = ""
+    for number in password:
+        encoded_password += str(int(number)+3) # Convert to integer for adding 3 then convert back to string to create encoded password
+    return encoded_password
+def menu():
+    print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+    option = int(input("Please enter an option: "))
+    return option
+
+# Program loop
+
+password_storage = "" # Variable for storing encoded passwords
+
+while True:
+    choice = menu()
+    if choice == 1:
+        password_storage = encode(input("Please enter your password: "))
+        print("Your password has been encoded and stored!\n")
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        break
